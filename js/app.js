0924aedc075567e98644559602dc877c652dd757
RhinoLee <picklerhino@gmail.com>
2021-01-01 03:28:30 +0800

    add main slider style

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 ;(function() {
   
   var mySwiper = new Swiper('.swiper-container', {
-    
-
+    autoplay: {
+      delay: 5000,
+    },
+    speed: 1500,
     loop: true,
     pagination: {
       el: '.swiper-pagination',
+      clickable: true
     },
   })
 
