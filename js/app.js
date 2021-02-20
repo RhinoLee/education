604b60b65c71e3f5d722a0e6df99148a20e9bf88
RhinoLee <picklerhino@gmail.com>
2021-02-20 19:10:14 +0800

    mobile add education & courese section

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 ;(function() {
   
-  var mySwiper = new Swiper('.swiper-container', {
+  let bannerSwiper = new Swiper('.main-slider .swiper-container', {
     // autoplay: {
     //   delay: 5000,
     // },
@@ -10,6 +10,27 @@
       el: '.swiper-pagination',
       clickable: true
     },
+  })
+
+  let coursesSwiper = new Swiper('#section-courses .swiper-container', {
+    // autoplay: {
+    //   delay: 5000,
+    // },
+    speed: 1500,
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+      }, 
+      720: {
+        slidesPerView: 3,
+      }, 
+    }
   })
 
 })();
