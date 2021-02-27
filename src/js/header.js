9f6662566e8d4910a3a0c84b2825409a206c0542
RhinoLee <picklerhino@gmail.com>
2021-02-27 14:48:25 +0800

    add dektop header animation

--- src/js/header.js
+++ src/js/header.js
@@ -1,39 +1,73 @@
 ;(function() {
   
   const body = document.querySelector('body')
+  const header = document.querySelector('header.main-header')
   const burgerMenu = document.querySelector('.bugger-menu')
   const firstLevelTitle = document.querySelectorAll('.first-title')
+  const firstLevelList = document.querySelectorAll('ul.first-level > li')
   const mask = document.querySelector('.mask')
+  let nowWidth = window.innerWidth
 
-  if(burgerMenu) {
+  const setWindowWidth = () => {
+    nowWidth = window.innerWidth
+  }
 
-    const menuHandler = (e) => {
-      if(body.classList.contains('menu-open')) {
-        body.classList.remove('menu-open')
-        body.style.overflow = 'scroll'
-      }else {
-        body.classList.add('menu-open')
-        body.style.overflow = 'hidden'
-      }
+  const menuHandler = (e) => {
+    if(body.classList.contains('menu-open')) {
+      body.classList.remove('menu-open')
+      body.style.overflow = 'scroll'
+    }else {
+      body.classList.add('menu-open')
+      body.style.overflow = 'hidden'
     }
+  }
 
-    const secondMenuHandler = (e) => {
+  const mobileSecondMenu = (e) => {
+    if(e.currentTarget.nextElementSibling.classList) {
       e.preventDefault();
-      if (e.currentTarget.nextElementSibling){
-        e.currentTarget.nextElementSibling.classList.toggle('open')
-      }
+      e.currentTarget.nextElementSibling.classList.toggle('open')
     }
+  }
 
-    firstLevelTitle.forEach( title => {
-      if (title.nextElementSibling){
-        title.addEventListener('click', secondMenuHandler)
-      }
-    })
-
-
-    burgerMenu.addEventListener('click', menuHandler)
-    mask.addEventListener('click', menuHandler)
+  const desktopSecondMenu = (e) => {
+    
+    if(e.type === "mouseenter") {
+      e.currentTarget.querySelector("ul.second-level").classList.add('open-animate')
+    }else{
+      e.currentTarget.querySelector("ul.second-level").classList.remove('open-animate')
+    }
 
   }
+
+  const setHeaderBackground = (e) => {
+    console.log(window.pageYOffset);
+    if(window.pageYOffset > 10) {
+      header.classList.add('bg')
+    }else {
+      header.classList.remove('bg')
+    }
+  }
+
+  firstLevelTitle.forEach( title => {
+    if (title.nextElementSibling){
+      title.addEventListener('click', mobileSecondMenu)
+    }
+  })
+
+  firstLevelList.forEach( list => {
+
+    if( list.querySelector("ul.second-level")){
+      list.addEventListener('mouseenter', desktopSecondMenu)
+      list.addEventListener('mouseleave', desktopSecondMenu)
+    }
+
+  })
+
+  
+  burgerMenu.addEventListener('click', menuHandler)
+  mask.addEventListener('click', menuHandler)
+
+  window.addEventListener('scroll', setHeaderBackground)
+  window.addEventListener('resize', setWindowWidth)
 
 })();
